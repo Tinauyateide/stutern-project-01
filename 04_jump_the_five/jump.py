63302ae76b832f8de3fddbfc811b1dea867b760c
Tinauyateide <tinauyats@gmail.com>
2022-07-21 18:57:35 +0100

    complete chapter 4

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Author : Tina Uyateide <tinauyat@gmail.com>
+Date   : 2022-07-18
+Purpose: JUMP THE FIVE
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='JUMP THE FIVE',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='str',
+                        help='Input text')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
+              '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
+
+    for char in args.text:
+        print(jumper.get(char, char), end='')
+
+    print()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
